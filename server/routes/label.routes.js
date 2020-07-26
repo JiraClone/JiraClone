b117c15767197ae52a07c068be7368cd63a871e3
dathanwong <dwong5@villanova.edu>
2020-07-26 11:35:32 -0700

    refactored object models to be more in line with NoSQL architecture

--- server/routes/label.routes.js
+++
@@ -1,9 +0,0 @@
-const LabelController = require('../controllers/label.controller');
-
-module.exports = function(app){
-    app.post('/api/labels', LabelController.create);
-    app.get('/api/labels', LabelController.findAll);
-    app.delete('/api/labels/:id', LabelController.delete);
-    app.put('/api/labels/:id', LabelController.update);
-    app.get('/api/labels/:id', LabelController.findById);
-}
