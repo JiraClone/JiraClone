2c6a91e7be1feaff0bac021eea5a0cb6a0a243a8
dathanwong <dwong5@villanova.edu>
2020-07-25 23:32:33 -0700

    Added task model

--- server/models/task.model.js
+++ server/models/task.model.js
@@ -1,19 +1,45 @@
 const mongoose = require('mongoose');
+const { User } = require('./user.model');
+const {Message} = require('./message.model');
 
 const TaskSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, "Name is required"]
     },
-    tasks:{
-        type: [User]
+    description:{
+        type: String
     },
-    users:{
-        type: [User]
+    comments:{
+        type: [Message]
+    },
+    type:{
+        type: String
     },
     dueDate:{
         type: Date
-    }
+    },
+    priority:{
+        type: String
+    },
+    assignee:{
+        type: User
+    },
+    creator:{
+        type: User
+    },
+    estimate:{
+        type: Number
+    },
+    timeTracked:{
+        type: Number
+    },
+    labels:{
+        type: String
+    },
+    status:{
+        type: String
+    },
 }, {timestamps: true});
 
 module.exports.Task = mongoose.model('Task', TaskSchema);
