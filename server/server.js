b71d08af1beab311b8e8634e55513102d47f0c80
MorningReference <aricjeon@gmail.com>
2020-07-26 12:22:04 -0700

    test from aric

--- server/server.js
+++ server/server.js
@@ -6,9 +6,11 @@ const port = 8000;
 require('../server/config/mongoose.config');
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 require('./routes/user.routes')(app);
 require('./routes/project.routes')(app);
 require('./routes/task.routes')(app);
-    
-app.listen(port, () => console.log(`Listening on port: ${port}`) );
+
+app.listen(port, () => console.log(`Listening on port: ${port}`));
+
+//this is only for a test.
