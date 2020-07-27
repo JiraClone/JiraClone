c133005b6c6a90e1fb4c3e34fa1ff717dead13e0
MorningReference <aricjeon@gmail.com>
2020-07-27 14:38:28 -0700

    Completed Prototype of TaskContent Page and added TaskContent component to the app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import TaskContent from './components/TaskContent';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <TaskContent />
+        </div>
+    );
 }
 
 export default App;
