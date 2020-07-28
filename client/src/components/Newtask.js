932494d8c0bf8f8ce3434439d59ce7687251f8fc
MorningReference <aricjeon@gmail.com>
2020-07-28 12:24:09 -0700

    Created taskNumber state in Main view, setting the state in Issue component, and using it in the TaskContent component

--- client/src/components/Newtask.js
+++ client/src/components/Newtask.js
@@ -23,7 +23,9 @@ export default function NewTask() {
 
     useEffect(() => {
         axios
-            .get('http://localhost:8000/api/projects')
+            .get('http://localhost:8000/api/projects', {
+                withCredentials: true,
+            })
             .then((res) => setProjects(res.data))
             .catch(console.log);
     });
@@ -47,8 +49,13 @@ export default function NewTask() {
         };
 
         axios
-            .post('http://localhost:8000/api/tasks', newTask)
-            .then((res) => res.data)
+            .post('http://localhost:8000/api/tasks/', newTask, {
+                withCredentials: true,
+            })
+            .then((res) => {
+                console.log('Successfully created new task! : ', res.data);
+                return res.data;
+            })
             .catch((err) => {
                 const errorResponse = err.response.data.errors;
                 const errorArr = [];
@@ -61,6 +68,11 @@ export default function NewTask() {
 
     return (
         <form className="form" onSubmit={handleSubmit}>
+            {/* {errors.map((err, idx) => (
+                <p key={idx} className="text-danger">
+                    {err}
+                </p>
+            ))} */}
             {/* <div className="form-group">
                 <label>Project</label>
                 <select
--- client/src/views/Main.js
+++ client/src/views/Main.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
 import TaskContent from '../components/TaskContent';
 import NewTask from '../components/NewTask';
+import Issues from '../components/Issues';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 export default function Main() {
+    const [taskNum, setTaskNum] = useState(0);
+    // function reducer(state, action) {
+    //     switch (action.type) {
+    //         case 'TASK_NUMBER':
+    //             return {
+    //                 ...state,
+    //                 task: action.task,
+    //             };
+    //         default:
+    //             return state;
+    //     }
+    // }
+
+    // const initialState = {
+    //     task: {
+    //         number: 0,
+    //     },
+    // };
+    // const store = createStore(reducer, initialState);
+
     return (
         <div>
+            {/* <Provider store={store}> */}
             <Header />
             <Sidebar />
-            <NewTask />
-            <TaskContent />
+            <Issues setTaskNumber={setTaskNum} />
+            {/* <NewTask /> */}
+            <TaskContent taskNumber={taskNum} />
+            {/* </Provider> */}
         </div>
     );
 }
