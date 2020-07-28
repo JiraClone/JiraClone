b14034470011b9244973af55a2e44059ff17414c
MorningReference <aricjeon@gmail.com>
2020-07-28 12:36:47 -0700

    Added another route that accesses the main view through /home/geer/:id in App.js, Main view initializes taskNum state with props.id

--- client/src/App.js
+++ client/src/App.js
@@ -2,32 +2,34 @@ import React from 'react';
 import './App.css';
 import Login from './views/Login';
 import Registration from './views/Registration';
-import {Router, Redirect, navigate} from '@reach/router';
+import { Router, Redirect, navigate } from '@reach/router';
 import Main from './views/Main';
 import Header from './components/Header';
 import Axios from 'axios';
 
 //Handles redirect if user is not logged in
-Axios.interceptors.response.use(res => res, 
-  err =>{
-    console.log(err.response.data);
-    navigate("/login");
-    return err;
-});
+Axios.interceptors.response.use(
+    (res) => res,
+    (err) => {
+        console.log(err.response.data);
+        navigate('/login');
+        return err;
+    }
+);
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Redirect from="/" to="/register" noThrow />
-        <Registration path="/register"/>
-        <Login path="/login"/>
-        <Main path="/home"/>
-        <Header path="/header"/>
-      </Router>
-    </div>
-  );
-
+    return (
+        <div className="App">
+            <Router>
+                <Redirect from="/" to="/register" noThrow />
+                <Registration path="/register" />
+                <Login path="/login" />
+                <Main path="/home" />
+                <Main path="/home/geer/:id" />
+                <Header path="/header" />
+            </Router>
+        </div>
+    );
 }
 
 export default App;
--- client/src/views/Main.js
+++ client/src/views/Main.js
@@ -4,39 +4,17 @@ import Sidebar from '../components/Sidebar';
 import TaskContent from '../components/TaskContent';
 import NewTask from '../components/NewTask';
 import Issues from '../components/Issues';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
 
-export default function Main() {
-    const [taskNum, setTaskNum] = useState(0);
-    // function reducer(state, action) {
-    //     switch (action.type) {
-    //         case 'TASK_NUMBER':
-    //             return {
-    //                 ...state,
-    //                 task: action.task,
-    //             };
-    //         default:
-    //             return state;
-    //     }
-    // }
-
-    // const initialState = {
-    //     task: {
-    //         number: 0,
-    //     },
-    // };
-    // const store = createStore(reducer, initialState);
+export default function Main(props) {
+    const [taskNum, setTaskNum] = useState(props.id);
 
     return (
         <div>
-            {/* <Provider store={store}> */}
             <Header />
             <Sidebar />
             <Issues setTaskNumber={setTaskNum} />
             {/* <NewTask /> */}
             <TaskContent taskNumber={taskNum} />
-            {/* </Provider> */}
         </div>
     );
 }
