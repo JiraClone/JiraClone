79c85ebc0fd3fd4d722a9f1c5dc2dfc8107a4e39
dathanwong <dwong5@villanova.edu>
2020-07-28 14:15:08 -0700

    When project is created now it will automatically add the creator as a user on that project and projects only shows projects that the user is a part of

--- server/controllers/project.controller.js
+++ server/controllers/project.controller.js
@@ -14,6 +14,12 @@ module.exports.findAll = (req, res)=>{
         .catch(err => res.json(err));
 }
 
+module.exports.findByUserId = (req, res) =>{
+    Project.find({users: req.params.user})
+        .then(projects => res.json(projects))
+        .catch(err => res.json(err));
+}
+
 module.exports.findById =(req, res)=>{
     Project.findOne({_id: req.params.id})
         .then(project => res.json(project))
@@ -28,6 +34,12 @@ module.exports.delete = (req, res) =>{
 
 module.exports.update = (req, res) =>{
     Project.updateOne({_id: req.params.id}, req.body, {new:true, runValidators: true})
+        .then(r => res.json(r))
+        .catch(err => res.status(400).json(err));
+}
+
+module.exports.addUser = (req, res) =>{
+    Project.updateOne({_id: req.params.id}, {$push: {users: req.params.user}}, {new:true})
         .then(r => res.json(r))
         .catch(err => res.status(400).json(err));
 }
